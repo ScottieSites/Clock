7e793469378f0f793758344772115d15b3309e83
Anang Williams <anangwilliams@gmail.com>
2020-08-24 19:51:07 -0400

    Third commit

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let today = new Date();
 
-function displayTime () {
 const time = 
 today.getHours() + ':' + 
 today.getMinutes() + ':'+
 today.getSeconds();
-};
+;
+
+let showTime = document.querySelectorAll('span');
+
+for(let span of showTime) {
+    span.innerHTML = `${time}`;
+}
+
+
+
